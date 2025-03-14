c671b27f7240c7c9f5c24c348da5323fc4a26b81
HasnainAliPK <hasnain2640@hotmail,com>
2025-03-14 09:43:43 +0100

    Added MC class with unit tests

---
+++ Bridge/MC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    /// <summary>
+    /// Represents a motorcycle crossing the bridge
+    /// </summary>
+    public class MC
+    {
+        /// <summary>
+        /// The license plate of the motorcycle
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// The date when the motorcycle crosses the bridge
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returns the price for crossing the bridge
+        /// </summary>
+        /// <returns>Fixed price of 120 DKK.</returns>
+        public double Price()
+        {
+            return 120.0;
+        }
+
+        /// <summary>
+        /// Returns the vehicle type
+        /// </summary>
+        /// <returns>String "MC"</returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ BridgeTests/MCTests.cs
@@ -0,0 +1,39 @@
+﻿using Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeTests
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void Price_Returns_Fixed_Price_120()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            double result = mc.Price();
+
+            // Assert
+            Assert.AreEqual(120.0, result);
+        }
+
+        [TestMethod]
+        public void VehicleType_Returns_MC()
+        {
+            // Arrange
+            var mc = new MC();
+
+            // Act
+            string result = mc.VehicleType();
+
+            // Assert
+            Assert.AreEqual("MC", result);
+        }
+    }
+}
